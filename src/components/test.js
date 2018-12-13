dfc1479486b5b04cbc7353bbc9a9788743be74b3
Netanel <nmurciano1@gmail.com>
2018-12-13 22:02:35 +0200

    test from vscode

--- src/components/test.js
+++ src/components/test.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import { View, Test } from 'react-native';
+
+cont test = () => {
+    return(
+        <View>
+            <Text>
+                ddd dddd
+            </Text>
+        </View>
+    )
+}
